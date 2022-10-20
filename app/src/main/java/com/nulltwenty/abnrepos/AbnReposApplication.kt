6014372063f97176aa08b7df2353676d8ebd00d2
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-20 12:14:32 +0200

    introduce strict mode

--- app/src/main/java/com/nulltwenty/abnrepos/AbnReposApplication.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/AbnReposApplication.kt
@@ -1,7 +1,21 @@
 package com.nulltwenty.abnrepos
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class AbnReposApplication : Application()
+class AbnReposApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder().detectAll().penaltyLog().penaltyDialog().build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder().detectAll().penaltyLog().build()
+        )
+    }
+}
