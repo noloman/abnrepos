ee2ed31a65fcee908fef279d66ec9691c796ff4f
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-20 12:32:29 +0200

    fix problem with fragment being instantiated twice

--- app/src/main/java/com/nulltwenty/abnrepos/ui/MainActivity.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/ui/MainActivity.kt
@@ -10,7 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainerView, RepositoriesListFragment.newInstance()).commit()
     }
 }
--- app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoriesListFragment.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoriesListFragment.kt
@@ -27,8 +27,10 @@ class RepositoriesListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.repositoryListRecyclerView)
         val loadingProgressIndicator =
             view.findViewById<CircularProgressIndicator>(R.id.loadingProgressIndicator)
@@ -40,7 +42,6 @@ class RepositoriesListFragment : Fragment() {
                 observeAndCollectChanges(loadingProgressIndicator, adapter)
             }
         }
-        return view
     }
 
     private suspend fun observeAndCollectChanges(
