9a91ae57e2b3389a233241ddfa7aa4f7bfa1b8f9
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-20 12:43:58 +0200

    remove unneeded code

--- app/src/main/java/data/api/service/GithubService.kt
+++ app/src/main/java/data/api/service/GithubService.kt
@@ -1,38 +1,17 @@
 package data.api.service
 
 import data.api.model.RepositoryListResponse
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GithubService {
     @GET("repos")
     suspend fun fetchRepos(
-        @Query("q") query: String,
-        @Query("page") page: Int,
-        @Query("per_page") itemsPerPage: Int
+        @Query("q") query: String, @Query("page") page: Int, @Query("per_page") itemsPerPage: Int
     ): Response<RepositoryListResponse>
 
     companion object {
         const val BASE_URL = "https://api.github.com/users/abnamrocoesd/"
-
-        fun create(): GithubService {
-            val logger = HttpLoggingInterceptor()
-            logger.level = HttpLoggingInterceptor.Level.BASIC
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build()
-                .create(GithubService::class.java)
-        }
     }
 }
