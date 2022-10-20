a3c0985db2f67de59c3b44a8cfd6f4261011f557
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-20 22:20:13 +0200

    rename

--- app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoryListFragment.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoryListFragment.kt
@@ -20,7 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class RepositoriesListFragment : Fragment() {
+class RepositoryListFragment : Fragment() {
     private val viewModel: RepositoriesListViewModel by viewModels()
 
     override fun onCreateView(
