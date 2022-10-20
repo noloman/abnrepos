174b86aefb64d66c32caf8f74e2711ab44764181
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-20 12:25:00 +0200

    use default coroutine dispatcher

--- app/src/main/java/com/nulltwenty/abnrepos/domain/RepositoryListUseCase.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/domain/RepositoryListUseCase.kt
@@ -1,14 +1,18 @@
 package com.nulltwenty.abnrepos.domain
 
+import com.nulltwenty.abnrepos.data.di.DefaultCoroutineDispatcher
 import com.nulltwenty.abnrepos.data.repository.RepositoryListRepository
 import com.nulltwenty.abnrepos.domain.model.AbnRepo
 import com.nulltwenty.abnrepos.domain.model.ResultOf
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class RepositoryListUseCase @Inject constructor(private val repositoryListRepository: RepositoryListRepository) {
-    suspend operator fun invoke(): ResultOf<List<AbnRepo>> = withContext(Dispatchers.IO) {
+class RepositoryListUseCase @Inject constructor(
+    private val repositoryListRepository: RepositoryListRepository,
+    @DefaultCoroutineDispatcher private val coroutineDispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(): ResultOf<List<AbnRepo>> = withContext(coroutineDispatcher) {
         repositoryListRepository.fetchRepositoryList()
     }
 }
