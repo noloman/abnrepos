c9d5af8ede4d2931b565b7b4723a45529ba7bf21
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-23 11:02:24 +0200

    add RepositoryDao test

---
+++ app/src/androidTest/java/com/nulltwenty/abnrepos/data/db/RepositoryDaoTest.kt
@@ -0,0 +1,38 @@
+package com.nulltwenty.abnrepos.data.db
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.PagingSource
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.nulltwenty.abnrepos.fakeRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class RepositoryDaoTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    private lateinit var repositoryDao: RepositoryDao
+    private val inMemoryDatabase = Room.inMemoryDatabaseBuilder(
+        ApplicationProvider.getApplicationContext(), RepositoriesDatabase::class.java
+    ).build()
+
+    @Before
+    fun setup() {
+        repositoryDao = inMemoryDatabase.repositoriesDao()
+    }
+
+    @Test
+    fun givenAListOfRepositories_whenInsertingThemIntoDatabase_itShouldRetrieveThem() = runTest {
+        val repositoryList = listOf(fakeRepository)
+        repositoryDao.insertAll(repositoryList)
+
+        val allRepositories: PagingSource<Int, Repository> = repositoryDao.allRepositories()
+
+        assertNotNull(allRepositories)
+    }
+}
