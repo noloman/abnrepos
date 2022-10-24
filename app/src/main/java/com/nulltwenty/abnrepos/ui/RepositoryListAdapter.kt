40e12f8da3041352bc6314da75326970da653f20
Manuel Lorenzo <manulorenzop@gmail.com>
2022-10-24 16:19:36 +0200

    add databinding and use it in the list adapter

--- app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoryListAdapter.kt
+++ app/src/main/java/com/nulltwenty/abnrepos/ui/RepositoryListAdapter.kt
@@ -1,42 +1,39 @@
 package com.nulltwenty.abnrepos.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
-import com.nulltwenty.abnrepos.R
+import com.nulltwenty.abnrepos.databinding.RepositoryItemRowBinding
 import com.nulltwenty.abnrepos.domain.model.AbnRepo
 
 class RepositoryListAdapter(private val onClickListener: (AbnRepo) -> Unit) :
     PagingDataAdapter<AbnRepo, RepositoryListAdapter.RepositoryViewHolder>(
         RepositoryDiffCallback
     ) {
-    inner class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val repositoryName: TextView = itemView.findViewById(R.id.repositoryName)
-        private val repositoryVisibility: TextView =
-            itemView.findViewById(R.id.repositoryVisibility)
-        private val ownerAvatar: ImageView = itemView.findViewById(R.id.repositoryOwnerAvatar)
+    private lateinit var binding: RepositoryItemRowBinding
 
+    inner class RepositoryViewHolder(private val binding: RepositoryItemRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(repo: AbnRepo) {
-            repositoryName.text = repo.name
-            repositoryVisibility.text = repo.visibility
-            ownerAvatar.load(repo.avatarUrl) {
-                crossfade(true)
-                transformations(CircleCropTransformation())
+            with(binding) {
+                repositoryName.text = repo.name
+                repositoryVisibility.text = repo.visibility
+                repositoryOwnerAvatar.load(repo.avatarUrl) {
+                    crossfade(true)
+                    transformations(CircleCropTransformation())
+                }
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.repository_item_row, parent, false)
-        return RepositoryViewHolder(view)
+        binding =
+            RepositoryItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RepositoryViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
